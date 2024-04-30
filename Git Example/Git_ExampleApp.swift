fbdc3ff9441c74b0b83b7aea2ae2dc3fcf855b7b
Anh Dinh <anhdinh17@gmail.com>
2024-04-30 17:57:37 -0500

    Initial Commit

---
+++ Git Example/Git_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ExampleApp.swift
+//  Git Example
+//
+//  Created by Anh Dinh on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
