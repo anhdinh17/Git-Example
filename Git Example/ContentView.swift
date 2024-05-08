adf235f054db2764614ee223f7889ab2ba090bf6
Anh Dinh <anhdinh17@gmail.com>
2024-05-08 09:23:18 -0500

    Kelly changed the text

--- Git Example/ContentView.swift
+++ Git Example/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 .frame(width: 150, height: 150)
                 .foregroundStyle(Color.green)
             
-            Text("Add new Explore View")
+            Text("The text is done by Kelly")
         }
         .padding()
         .onAppear {
